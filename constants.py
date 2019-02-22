137bfb2fcda704067cbcfe3ab8c5951b785d4172
Shubham Kumar <35415266+UnresolvedCold@users.noreply.github.com>
2019-02-22 22:34:03 +0530

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,22 @@
+A_MENU_VAR = 'a_menu'
+C_MENU_VAR = 'c_menu'
+ITEMS_VAR = 'items'
+A_MENU_VAR_ITEMS = {
+    'Veg Triple Rice (Rs.55)' : 55,
+    'ABCDFEF': 30
+}
+C_MENU_VAR_ITEMS = {
+    'Veg Triple Rice (Rs.55)': 55
+}
+DELIVERY_CHARGE = 10
+PACKING_CHARGE = 10
+MENU_VAR = 'menu'
+INITIAL_COST_VAR = 'intitial_cost'
+FINAL_COST_VAR = 'final_cost'
+DELIVERY_CHARGE_VAR = 'delivery_charge'
+PACKING_CHARGE_VAR = 'packing_charge'
+NAME_VAR = 'name'
+ROOM_VAR = 'room'
+PHONE_VAR = 'phone'
+ORDER_ID_VAR = 'order_id'
+PAYMENT_MODE_VAR = 'payment_mode'
